7d0db764d163554e94375f4cfc71d0a193105474
Maksym <maksymsydorovych@gmail.com>
2023-11-21 23:57:18 +0100

    reset styles

--- src/components/dice/dice.jsx
+++ src/components/dice/dice.jsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 
 const Dice = ({ onRoll }) => {
     const [value, setValue] = useState(1);
-
+    const [diceValue, setDiceValue] = useState(0);
     const rollDice = () => {
         const newValue = Math.floor(Math.random() * 6) + 1;
         setValue(newValue);
         onRoll(newValue);
+        setDiceValue(value);
+
     };
 
     return (
