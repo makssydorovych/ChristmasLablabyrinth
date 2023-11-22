45b0b3bddc77b1736ba917951567910bdb18492a
Maksym <maksymsydorovych@gmail.com>
2023-11-22 16:51:20 +0100

    feat GameSlice logick

--- src/services/game.slice.js
+++ src/services/game.slice.js
@@ -2,22 +2,41 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     difficulty: 'easy',
-    playerNames: [],
+    players: []
 };
-
 export const gameSlice = createSlice({
     name: 'game',
     initialState,
     reducers: {
+
         setDifficulty: (state, action) => {
             state.difficulty = action.payload;
         },
-        setPlayerNames: (state, action) => {
-            state.playerNames = action.payload;
+
+        addPlayer: (state, action) => {
+            if (state.players.length < 6) {
+                state.players.push({
+                    name: action.payload.name,
+                    position: { row: state.players.length, col: 0 },
+                    isActive: false
+                });
+            }
         },
+
+        updatePlayer: (state, action) => {
+            const { playerIndex, name, position, isActive } = action.payload;
+            if (state.players[playerIndex]) {
+                state.players[playerIndex].name = name ?? state.players[playerIndex].name;
+                state.players[playerIndex].position = position ?? state.players[playerIndex].position;
+                state.players[playerIndex].isActive = isActive ?? state.players[playerIndex].isActive;
+            }
+        },
+
     },
 });
 
-export const { setDifficulty, setPlayerNames } = gameSlice.actions;
+
+export const { setDifficulty, addPlayer, updatePlayer } = gameSlice.actions;
+
 
 export default gameSlice.reducer;
