794ff117daf3df2bfc86030d3cb13d278f056e8a
Maksym <maksymsydorovych@gmail.com>
2023-11-22 17:07:23 +0100

    feat GameBoard

---
+++ src/pages/gameBoard/game-board.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Dice from "../../components/dice/dice.jsx";
+import Labyrinth from "../../components/labyrinth/labyrinth.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { gameSlice } from "../../services/game.slice.js";
+import Players from "../../components/player/player.jsx";
+
+const GameBoard = () => {
+
+
+    return (
+        <div>
+GAMEBOARD!!!!!!!!!
+        </div>
+    );
+};
+
+export default GameBoard;
