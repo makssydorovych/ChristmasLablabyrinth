e11f93ebc8e6480b2d6cc500342d2e3f0b9f9951
Maksym <maksymsydorovych@gmail.com>
2023-11-23 12:59:12 +0100

    feat Dice

--- src/components/dice/dice.jsx
+++ src/components/dice/dice.jsx
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
-const Dice = ({ onRoll }) => {
-    const [value, setValue] = useState(1);
-    const [diceValue, setDiceValue] = useState(0);
-    const rollDice = () => {
-        const newValue = Math.floor(Math.random() * 6) + 1;
-        setValue(newValue);
-        onRoll(newValue);
-        setDiceValue(value);
+const Dice = () => {
+    const players = useSelector((state) => state.game.players);
+    const activePlayerIndex = players.findIndex(player => player.isActive);
+    const activePlayer = players[activePlayerIndex];
+    const [isDiceActive, setIsDiceActive] = useState(false);
+    const [diceValue, setDiceValue] = useState(1);
 
+    useEffect(() => {
+        if (activePlayer) {
+            setIsDiceActive(true);
+        }
+    }, [activePlayer]);
+
+    const handleRollDice = () => {
+        if (isDiceActive) {
+            const newValue = Math.ceil(Math.random() * 6); // Генерация числа от 1 до 6
+            setDiceValue(newValue);
+            setIsDiceActive(false); // Деактивация кубика после броска
+        }
     };
+
+    const diceColor = activePlayerIndex >= 0 ? `var(--color-player-${activePlayerIndex + 1})` : 'grey';
 
     return (
         <div>
-            <button onClick={rollDice}>Throw dice</button>
-            <p>Number: {value}</p>
+            {isDiceActive && (
+                <button
+                    style={{ backgroundColor: diceColor }}
+                    onClick={handleRollDice}>
+                    Roll Dice
+                </button>
+            )}
+            <p>Dice Value: {diceValue}</p>
         </div>
     );
 };
