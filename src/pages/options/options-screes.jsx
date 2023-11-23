38c8288b57050da810ef27afcd708666f7561e18
Maksym <maksymsydorovych@gmail.com>
2023-11-23 22:53:41 +0100

    feat: OptionsScreen animations and styles

---
+++ src/pages/options/options-screes.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addPlayer } from "../../services/game.slice.js";
+import Players from "../../components/player/players.jsx";
+import { useNavigate } from "react-router-dom";
+import s from "./options-screen.module.scss";
+
+const OptionsScreen = () => {
+  const [playerName, setPlayerName] = useState("");
+  const dispatch = useDispatch();
+  const players = useSelector((state) => state.game.players);
+  const navigate = useNavigate();
+
+  const handleAddPlayer = () => {
+    if (playerName.trim()) {
+      dispatch(addPlayer({ name: playerName }));
+      setPlayerName("");
+    }
+  };
+
+  const handleStartGame = () => {
+    if (players.length > 0) {
+      // dispatch(updatePlayer({ playerIndex: 0, isActive: true }));
+      navigate("/game");
+    }
+  };
+
+  return (
+    <div className={s.optionsScreen}>
+      <Players names={players.map((player) => player.name)} />
+      <input
+        className={s.optionsScreenInput}
+        type="text"
+        placeholder="Enter player name"
+        value={playerName}
+        onChange={(e) => setPlayerName(e.target.value)}
+      />
+      <button className={s.optionsScreenButton} onClick={handleAddPlayer}>
+        Add Player
+      </button>
+      <button className={s.optionsScreenButton} onClick={handleStartGame}>
+        Start Game
+      </button>
+    </div>
+  );
+};
+
+export default OptionsScreen;
