e6c3caf1398857f76479219d7f2ca4f4d0750a90
Maksym <maksymsydorovych@gmail.com>
2023-11-26 14:04:22 +0100

    style: DiceStyle and animations

--- src/components/dice/dice.jsx
+++ src/components/dice/dice.jsx
@@ -1,43 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {setDiceValue} from "../../services/game.slice.js";
-
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setDiceValue } from "../../services/game.slice.js";
+import s from "./dice.module.scss";
+import { motion } from "framer-motion";
 const Dice = () => {
-    const players = useSelector((state) => state.game.players);
-    const diceValue = useSelector((state) => state.game.diceValue);
-    const activePlayerIndex = players.findIndex(player => player.isActive);
-    const activePlayer = players[activePlayerIndex];
-    const [isDiceActive, setIsDiceActive] = useState(false);
-
-const dispatch = useDispatch();
-    useEffect(() => {
-        if (activePlayer) {
-            setIsDiceActive(true);
-        }
-    }, [activePlayer]);
-
-    const handleRollDice = () => {
-        if (isDiceActive) {
-            const newValue = Math.ceil(Math.random() * 6);
-            dispatch(setDiceValue(newValue)); // Обновляем значение кубика через Redux
-            setIsDiceActive(false);
-        }
-    };
-
-    const diceColor = activePlayerIndex >= 0 ? `var(--color-player-${activePlayerIndex + 1})` : 'grey';
-    const displayDiceValue = typeof diceValue === 'object' ? diceValue.diceValue : diceValue;
-    return (
-        <>
-            {isDiceActive && (
-                <button
-                    style={{ backgroundColor: diceColor }}
-                    onClick={handleRollDice}>
-                    Roll Dice
-                </button>
-            )}
-            <p>Dice Value: {displayDiceValue}</p>
-        </>
-    );
+  const players = useSelector((state) => state.game.players);
+  const diceValue = useSelector((state) => state.game.diceValue);
+  const activePlayerIndex = players.findIndex((player) => player.isActive);
+  const activePlayer = players[activePlayerIndex];
+  const [isDiceActive, setIsDiceActive] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (activePlayer) {
+      setIsDiceActive(true);
+    }
+  }, [activePlayer]);
+  const handleRollDice = () => {
+    if (isDiceActive) {
+      const newValue = Math.ceil(Math.random() * 6);
+      dispatch(setDiceValue(newValue));
+      setIsDiceActive(false);
+    }
+  };
+  const diceColor =
+    activePlayerIndex >= 0
+      ? `var(--color-player-${activePlayerIndex + 1})`
+      : "grey";
+  const displayDiceValue =
+    typeof diceValue === "object" ? diceValue.diceValue : diceValue;
+  return (
+    <div className={s.dice}>
+      <motion.button
+        className={s.diceButton}
+        style={{ background: diceColor }}
+        onClick={handleRollDice}
+        whileTap={{ scale: 0.7 }}
+      >
+        Roll Dice
+      </motion.button>
+      <p className={s.diceValue} style={{ color: diceColor }}>
+        {displayDiceValue}
+      </p>
+    </div>
+  );
 };
 
 export default Dice;
