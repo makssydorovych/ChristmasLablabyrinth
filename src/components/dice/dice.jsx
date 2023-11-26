060d2ebab166ef3bfc31b86c12f1d24908dbf1ea
Maksym <maksymsydorovych@gmail.com>
2023-11-26 14:39:59 +0100

    style: Dice fix more adaptive

--- src/components/dice/dice.jsx
+++ src/components/dice/dice.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setDiceValue } from "../../services/game.slice.js";
 import s from "./dice.module.scss";
 import { motion } from "framer-motion";
+import Players from "../players/players.jsx";
 const Dice = () => {
   const players = useSelector((state) => state.game.players);
   const diceValue = useSelector((state) => state.game.diceValue);
@@ -30,14 +31,15 @@ const Dice = () => {
     typeof diceValue === "object" ? diceValue.diceValue : diceValue;
   return (
     <div className={s.dice}>
+      <Players onlyActive={true} />
       <motion.button
         className={s.diceButton}
         style={{ background: diceColor }}
         onClick={handleRollDice}
         whileTap={{ scale: 0.7 }}
       >
-        Roll Dice
-      </motion.button>
+        Roll !!!
+      </motion.button>{" "}
       <p className={s.diceValue} style={{ color: diceColor }}>
         {displayDiceValue}
       </p>
