303ea214f14a9095716ed27ca1ce4ddb333c1381
Maksym <maksymsydorovych@gmail.com>
2023-11-26 19:35:02 +0100

    style: Add fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.scss'
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./styles/index.scss";
+import App from "./App.jsx";
+import "@fontsource/dm-serif-text";
+import "@fontsource/ultra";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
