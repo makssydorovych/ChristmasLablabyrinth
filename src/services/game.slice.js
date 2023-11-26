9a65760b81b0db34bd7c642d371ecc6535447743
Maksym <maksymsydorovych@gmail.com>
2023-11-26 22:23:40 +0100

    feat: BLLofGame

--- src/services/game.slice.js
+++ src/services/game.slice.js
@@ -1,46 +1,59 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    difficulty: 'easy',
-    players: [],
-    diceValue: 0,
+  difficulty: "easy",
+  players: [],
+  diceValue: 0,
 };
+
 export const gameSlice = createSlice({
-    name: 'game',
-    initialState,
-    reducers: {
-
-        setDifficulty: (state, action) => {
-            state.difficulty = action.payload;
-        },
-
-        addPlayer: (state, action) => {
-            if (state.players.length < 6) {
-                state.players.push({
-                    name: action.payload.name,
-                    position: { row: state.players.length, col: 0 },
-                    isActive: false
-                });
-            }
-        },
-
-        updatePlayer: (state, action) => {
-            const { playerIndex, name, position, isActive } = action.payload;
-            if (state.players[playerIndex]) {
-                state.players[playerIndex].name = name ?? state.players[playerIndex].name;
-                state.players[playerIndex].position = position ?? state.players[playerIndex].position;
-                state.players[playerIndex].isActive = isActive ?? state.players[playerIndex].isActive;
-            }
-        },
-        setDiceValue: (state, action) => {
-            state.diceValue = action.payload;
-        },
-
+  name: "game",
+  initialState,
+  reducers: {
+    setDifficulty: (state, action) => {
+      state.difficulty = action.payload;
     },
+    addPlayer: (state, action) => {
+      if (state.players.length < 6) {
+        state.players.push({
+          name: action.payload.name,
+          position: { row: 0, col: state.players.length },
+          isActive: false,
+          snowflakes: 0,
+        });
+      }
+    },
+    updatePlayer: (state, action) => {
+      const { playerIndex, position, isActive, isFinished, name, snowflakes } =
+        action.payload;
+      const player = state.players[playerIndex];
+      if (player) {
+        player.isFinished = isFinished ?? player.isFinished;
+        player.name = name ?? player.name;
+        player.position = position ?? player.position;
+        player.isActive = isActive ?? player.isActive;
+        if (snowflakes !== undefined) {
+          player.snowflakes = snowflakes;
+        }
+      }
+    },
+    setDiceValue: (state, action) => {
+      state.diceValue = action.payload;
+    },
+    resetGame: (state) => {
+      state.players.forEach((player) => {
+        player.snowflakes = 0;
+      });
+    },
+  },
 });
 
-
-export const { setDifficulty, addPlayer, updatePlayer,setDiceValue } = gameSlice.actions;
-
+export const {
+  setDifficulty,
+  addPlayer,
+  updatePlayer,
+  setDiceValue,
+  resetGame,
+} = gameSlice.actions;
 
 export default gameSlice.reducer;
