0319cc61b184e596e97dc0bfb26f257314841cc5
Maksym <maksymsydorovych@gmail.com>
2023-11-26 14:12:06 +0100

    feat: Players: now can use for render only active player

--- src/components/players/players.jsx
+++ src/components/players/players.jsx
@@ -1,23 +1,42 @@
 import s from "./players.module.scss";
 import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
 
-const Players = () => {
+const Players = ({ onlyActive }) => {
   const players = useSelector((state) => state.game.players);
   const activePlayerIndex = players.findIndex((player) => player.isActive);
+  const playerBackgroundClass = s[`player-${activePlayerIndex + 1}`];
   return (
     <ol className={s.players}>
-      {players.map((player, index) => (
+      {onlyActive ? (
         <li
-          className={`${s.player} ${s[`player-${index + 1}`]} ${
-            index === activePlayerIndex ? s.active : ""
-          }`}
-          key={index}
+          className={`${s.player} ${playerBackgroundClass} ${s.active}`}
+          key={players[activePlayerIndex].name}
         >
-          {player.name}
+          {players[activePlayerIndex].name}
         </li>
-      ))}
+      ) : (
+        players.map((player, index) => (
+          <li
+            className={`${s.player} ${s[`player-${index + 1}`]} ${
+              index === activePlayerIndex ? s.active : ""
+            }`}
+            key={player.name}
+          >
+            {player.name}
+          </li>
+        ))
+      )}
     </ol>
   );
+};
+
+Players.propTypes = {
+  onlyActive: PropTypes.bool,
+};
+
+Players.defaultProps = {
+  onlyActive: false,
 };
 
 export default Players;
