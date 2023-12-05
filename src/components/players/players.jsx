1d15d94d91a4138722741fa5e6d3219a606e32d8
Maksym <maksymsydorovych@gmail.com>
2023-12-05 13:38:55 +0100

    feat: add snowflake to player

--- src/components/players/players.jsx
+++ src/components/players/players.jsx
@@ -1,20 +1,39 @@
 import s from "./players.module.scss";
 import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
+import Snowflake from "../../assets/images/snowflake.jsx";
 
 const Players = ({ onlyActive }) => {
   const players = useSelector((state) => state.game.players);
-  const activePlayerIndex = players.findIndex((player) => player.isActive);
-  const playerBackgroundClass = s[`player-${activePlayerIndex + 1}`];
+  const difficulty = useSelector((state) => state.game.difficulty);
+  const activePlayerIndex = players.findIndex(
+    (player) => player.isActive && !player.isFinished,
+  );
+
   return (
     <ol className={s.players}>
       {onlyActive ? (
-        <li
-          className={`${s.player} ${playerBackgroundClass} ${s.active}`}
-          key={players[activePlayerIndex].name}
-        >
-          {players[activePlayerIndex].name}
-        </li>
+        activePlayerIndex !== -1 && !players[activePlayerIndex].isFinished ? (
+          <li
+            className={`${s.player} ${s[`player-${activePlayerIndex + 1}`]} ${
+              s.active
+            }`}
+            key={players[activePlayerIndex].name}
+          >
+            {difficulty === "hard" ? (
+              <div className={s.playerWithSnowFlakes}>
+                {players[activePlayerIndex].name}
+
+                <Snowflake />
+                <div className={s.snowflakes}>
+                  {players[activePlayerIndex].snowflakes}
+                </div>
+              </div>
+            ) : (
+              <div>{players[activePlayerIndex].name}</div>
+            )}
+          </li>
+        ) : null
       ) : (
         players.map((player, index) => (
           <li
